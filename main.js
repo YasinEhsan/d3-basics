52689b6439cd1343c3ac0a12fe7f33909dbe7faa
yasinehsan <yasinehsan11@gmail.com>
2020-01-04 15:16:45 -0500

    added update and data joins

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-/*ABLE TO READ DATA EXCEL SHEET
-  - create contaoner > div > svg
-  - if pulling data do a trail catch for it if data not readily on server
-  - domain is the min  max of wtv looking at
-  - range is
-*/
-
 const body = d3.select('body')
 const container = body.append('div')
     .attr('class', 'container')
@@ -26,18 +19,14 @@ async function loadData(dataset) {
     })
     const xScaleDomain = data.map(d => d.name)
     const yScaleDomain = d3.extent(data.map(d => d.value))
-    console.log(yScaleDomain)
-
     const xScale = d3.scaleBand()
         .domain(xScaleDomain)
         .range([0, width])
         .padding(0.1)
-
     const yScale = d3.scaleLinear()
         .domain(yScaleDomain)
         .range([height, 0])
         .nice()
-
     const bars = gContainer.selectAll('rect')
         .data(data)
         .enter().append('rect')
@@ -46,16 +35,59 @@ async function loadData(dataset) {
         .attr('height', d => yScale(0) - yScale(d.value))
         .attr('x', d => xScale(d.name))
         .attr('y', d => yScale(d.value))
-
     const xAxis = d3.axisBottom(xScale)
-
     gContainer.append('g')
         .attr('class', 'x axis')
         .call(xAxis)
         .attr('transform', `translate(${0},${yScale(0)})`)
     const yAxis = d3.axisLeft(yScale)
-
     gContainer.append('g')
         .attr('class', 'y axis')
         .call(yAxis)
+    timedUpdate()
+    function timedUpdate() {
+        setTimeout(() => {
+        updateData()
+            timedUpdate()
+        }, 8500)
+    }
+    function updateData() {
+        const randNum = () => Math.floor(Math.random()*27)
+        const num = randNum()
+        console.log('num', num)
+        const numSet = new Set
+        let counter = 0
+        while (counter < num) { numSet.add(randNum()); counter++ }
+        const updatedData = data.filter((d, i) => numSet.has(i))
+        const t = gContainer.transition()
+            .duration(1100)
+        const update = gContainer.selectAll('rect')
+            .data(updatedData, d => d.name)
+            .join(
+                enter => enter.append('rect')
+                    .attr('x', d => xScale(d.name))
+                    .attr('width', xScale.bandwidth())
+                    .attr('y', d => yScale(d.value) - 200)
+                    .attr('height', d => yScale(0) - yScale(d.value))
+                    .attr('fill', 'white')
+                  .call(enter => enter.transition(t)
+                    .attr('y', d => yScale(d.value))
+                    .attr('fill', 'green')),
+                update => update
+                    .attr('fill', 'black'),
+                exit => exit
+                    .attr('fill', 'brown')
+                  .call(exit => exit.transition(t)
+                    .attr('y', d => yScale(d.value) + height * 1.5)
+                    .remove())
+            )
+        // const enter = update.enter()
+        // const exit = update.exit()
+        // console.log('update', update)
+        // console.log('enter', enter)
+        // console.log('exit', exit)
+        // console.log('---')
+        console.log('updatedData:')
+        console.table(updatedData)
+    }
 }
